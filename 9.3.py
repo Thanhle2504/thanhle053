a785b88d7e33e72863c55e3c29c06af4666c68a6
Thanhle2504 <117256417+Thanhle2504@users.noreply.github.com>
2022-12-15 22:24:47 +0700

    Update 9.3.py

--- 9.3.py
+++ 9.3.py
@@ -1,12 +1,16 @@
 #9.3
 import math
+def BMI(a,b):
+    return a/b*b
 a=float(input("Nhập cân nặng(kg):" ))
 b=float(input("Nhập chiều cao(m):"))
-BMI=a/(b*b)
-print("chỉ số BMI của bạn là:",BMI)
-if BMI<18.5:
-    print("Bạn gầy quá")
-elif BMI <=25:
-    print("Cơ thể của bạn bình thường")
-elif BMI>=25:
-    print("Bạn bị thừa cân")
+def phan_loai(BMI):
+    if BMI <18.5:
+        return "Bạn gầy quá"
+    elif BMI <=25:
+        return "Cơ thể của bạn bình thường"
+    elif BMI >=25:
+        return "Bạn bị thừa cân"
+bmi = BMI(a,b)
+print("Chi so co the cua ban la: ",bmi)
+print(phan_loai(bmi))
